f658f5ebd968bc109ee43ce2fd6af24c0835523b
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-04-04 12:51:54 +0200

    fixed bug

--- goenv/goenv.go
+++ goenv/goenv.go
@@ -6,14 +6,20 @@ import (
 	"github.com/spf13/viper"
 )
 
+type FileEnv struct {
+	Path string
+	Name string
+	Extn string
+}
+
 /*
    @notice Load env file into the programme.
    @param _path Path to env file (ex: . or ../).
    @param _name Name of env file (ex: secret).
    @param _type Extension of env file (ex: env).
 */
-func GetEnv(_path, _name, _type string) (interface{}, error) {
-	env, err := loadEnv(_path, _name, _type)
+func GetEnv(_f FileEnv, _result interface{}) (interface{}, error) {
+	env, err := loadEnv(_f, _result)
 
 	return env, err
 }
@@ -24,17 +30,27 @@ func GetEnv(_path, _name, _type string) (interface{}, error) {
    @param _name Name of env file (ex: secret).
    @param _type Extension of env file (ex: env).
 */
-func loadEnv(_path, _name, _type string) (env interface{}, err error) {
-	viper.AddConfigPath(_path)
-	viper.SetConfigName(_name)
-	viper.SetConfigType(_type)
+func loadEnv(_f FileEnv, _result interface{}) (interface{}, error) {
+
+	result := _result
+
+	viper.AddConfigPath(_f.Path)
+	viper.SetConfigName(_f.Name)
+	viper.SetConfigType(_f.Extn)
 
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
+	err := viper.ReadInConfig()
+
+	if err != nil {
 		log.Println(err.Error())
 	}
 
-	err = viper.Unmarshal(&env)
-	return
+	err = viper.Unmarshal(&result)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return result, err
 }
